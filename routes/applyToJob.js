b90c9fdb748306ef29301a9bcd6153ddc531bcb9
umairx97 <umair.bajwa@hotmail.com>
2019-08-31 16:45:29 +0500

    added apply to job api

--- routes/applyToJob.js
+++ routes/applyToJob.js
@@ -28,10 +28,56 @@ exports = module.exports = function (app, mongoose) {
     var upload = multer({ storage: storage, fileFilter: docFilter });
 
 
-
     router.post('/apply', upload.single('CV'), async (req, res) => {
-        
-        // const { jobId, email, cv }
+      
+        var { candidateEmail, jobId } = req.body
+
+        try {
+            if (!candidateEmail || !jobId) {
+                return res.send({ success: false, message: "Please Send Data" })
+            }
+
+            if (!req.file) {
+                return res.send({ success: false, message: "Please Send File" })
+            }
+
+            if (req.fileError) {
+                return res.send({ success: false, message: 'Wrong file selected' })
+            }
+
+            const foundJob = await app.db.models.Job.findOne({ _id: jobId })
+
+            if (!foundJob) {
+                return res.send({ success: false, message: 'No Job Found' })
+            }
+
+
+            var isUpdateAllowed = foundJob.CVS.every(item => candidateEmail !== item.email);
+
+
+            if (!isUpdateAllowed) {
+                return res.send({ success: false, message: "You have already applied on this job" })
+            }
+
+            const uploadedCV = await cloudinary.v2.uploader.upload(req.file.path, { resource_type: "auto" })
+            console.log(uploadedCV)
+
+            await app.db.models.Job.updateOne({ _id: jobId }, {
+                $addToSet: { CVS: { email: candidateEmail, cvLink: uploadedCV.secure_url } }
+
+            })
+                .then(updatedJob => {
+                    res.send({ success: true, message: "Applied" })
+                })
+                .catch(err => {
+                    console.log(err.message)
+                    res.send({ success: false, message: "Cannot apply to this job", err: err.message })
+                })
+
+        } catch (err) {
+            res.send({ success: false, message: "Something went wrong please try again later", err: err.message })
+        }
+
     });
 
     app.use('/job', router);
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ exports = module.exports = function (app, mongoose) {
 
   require('./home')(app, mongoose);
   require('./auth')(app, mongoose);
-  require("./addJob")(app, mongoose)
+  require("./addJob")(app, mongoose);
+  require("./applyToJob")(app, mongoose)
 
 };
