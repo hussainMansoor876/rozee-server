ddc8278c631ba096678d7fbb268c7b1f5cd6708e
umairx97 <umair.bajwa@hotmail.com>
2019-10-06 00:17:51 +0500

    fixed Email Functionality

--- config/index.js
+++ config/index.js
@@ -5,9 +5,8 @@ exports = module.exports = function (app) {
     console.log("process.env.cloudinary_key ========= ", process.env.cloudinary_key);
     console.log("process.env.cloudinary_secret ========= ", process.env.cloudinary_secret);
     console.log("process.env.sender_email ========= ", process.env.SENDER_EMAIL);
-    console.log("process.env.sender_email ========= ", process.env.RECIEVER_EMAIL);
-
-
+    console.log("process.env.sender_email ========= ", process.env.RECEIVER_EMAIL);
+    console.log("process.env.SENDGRID_API_KEY =========", process.env.SENDGRID_API_KEY);
 
     if (process.env.NODE_ENV === "production") {
         app.set("showLogs", true)
@@ -15,8 +14,7 @@ exports = module.exports = function (app) {
         app.set("cloudinary_name", process.env.cloudinary_name)
         app.set("cloudinary_key", process.env.cloudinary_key)
         app.set("cloudinary_secret", process.env.cloudinary_secret)
-        app.set('SENDER_EMAIL', "alitanveerahmad876@gmail.com")
-        app.set('RECIEVER_EMAIL', "alitanveerahmad876@gmail.com")
+        app.set("SENDGRID_API_KEY",process.env.SENDGRID_API_KEY)
 
 
     } else {
@@ -25,8 +23,8 @@ exports = module.exports = function (app) {
         app.set("cloudinary_name", "dklfq58uq")
         app.set("cloudinary_key", "731492993649535")
         app.set("cloudinary_secret", "nSfb5hNghIDf9GITZavliVERls4")
-        app.set('SENDER_EMAIL', "alitanveerahmad876@gmail.com")
-        app.set('RECIEVER_EMAIL', "alitanveerahmad876@gmail.com")
+        app.set('SENDGRID_API_KEY', "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")
+
 
     }
 }
--- routes/helpers/mailer.js
+++ routes/helpers/mailer.js
@@ -1,4 +1,4 @@
-exports = module.exports = function(app, mongoose){
+exports = module.exports = function (app, mongoose) {
 
     var express = require('express');
     var router = express.Router();
@@ -6,46 +6,41 @@ exports = module.exports = function(app, mongoose){
     const sgMail = require('@sendgrid/mail');
     let sendgrid_apikey = app.get('SENDGRID_API_KEY')
     sgMail.setApiKey(sendgrid_apikey);
+    const SENDER_EMAIL = "alitanveerahmad876@gmail.com";
+    const RECEIVER_EMAIL = "info@247itinternational.com";
 
 
     router.post('/', async (req, res) => {
-        console.log(app.get('SENDER_EMAIL'))
-        const {name, email, phone, message } = req.body
-        try{
-            if(!name || !email || !phone || !message){
-               return res.send("please send data")
+        const { name, email, phone, message } = req.body
+        try {
+            if (!name || !email || !phone || !message) {
+                return res.send("please send data")
             }
 
             const emailContent = {
-                from: app.get('SENDER_EMAIL'),
-                to: app.get('RECIEVER_EMAIL'),
+                from: { email: SENDER_EMAIL, name: "24/7 International" },
+                to: RECEIVER_EMAIL,
                 subject: "247 IT international help",
                 text: "Help message to 247 IT international",
-                html:`<h3>Email Address: <span> ${email} </span></h3>
+                html: `<h3>Email Address: <span> ${email} </span></h3>
                 </br>
-                <h3>User Name: <span> ${name} </span></h3>
+                <h3>Name: <span> ${name} </span></h3>
                 </br>
-                <h3>User Phone Number: <span> ${phone} </span></h3>
+                <h3>Phone Number: <span> ${phone} </span></h3>
                 </br>
-                <p> ${message}</p>` 
+                <h3>Message: <span> ${message} </span></h3>`
             };
 
             sgMail.send(emailContent).then(res => {
-                console.log(`LogLevel1 :: Email Sent to asadiklas03@gmail.com`);
+                console.log(`LogLevel1 :: Email Sent to ${RECEIVER_EMAIL}`);
             }).catch(err => {
                 console.log("Error in email sending ", err.message);
             });
+            res.send({ success: true, message: 'Your message is successfully submited to us' })
 
-            // formatTemplate = () => { 
-            //     const EMAIL_TEMPLATE =            
-                
-            //     return EMAIL_TEMPLATE
-            // }
-            
-            res.send({success:true, message: 'Your message is successfully submited to us'})
-            
-        } catch(err){
-            res.send({success: false, message:'something went wrong please try again later'})
+
+        } catch (err) {
+            res.send({ success: false, message: 'something went wrong please try again later' })
         }
     })
 
