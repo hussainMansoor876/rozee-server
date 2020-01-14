9c32129ebc37083314b2b0f32b7e48448c7ebe88
Mansoor <hussainmansoor876@gmail.com>
2020-01-14 22:52:44 +0500

    updated1

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var cors = require('cors')
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
@@ -17,52 +18,53 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
-app.use('*', (req, res, next) => {
+// app.use('*', (req, res, next) => {
 
 
-  console.log('Entering log level')
+//   console.log('Entering log level')
 
-  const domains = [
-    "http://localhost:3000",
-    'https://rozee-client.herokuapp.com',
-    'http://rozee-client.herokuapp.com',
-    "https://rozee-user.herokuapp.com",
-    'http://rozee-user.herokuapp.com/',
-    'http://rozeeadmin.codesation.com',
-    'http://rozee.codesation.com/',
-    'http://247itinternational.com',
-    'http://247itinternational.co.uk',
-    'http://admin.247itinternational.com',
-    'http://www.247itinternational.com/'    
-  ]
-  var origin = req.headers.origin;
-  app.logLevel1("origin >>> ", origin);
+//   const domains = [
+//     "http://localhost:3000",
+//     'https://rozee-client.herokuapp.com',
+//     'http://rozee-client.herokuapp.com',
+//     "https://rozee-user.herokuapp.com",
+//     'http://rozee-user.herokuapp.com/',
+//     'http://rozeeadmin.codesation.com',
+//     'http://rozee.codesation.com/',
+//     'http://247itinternational.com',
+//     'http://247itinternational.co.uk',
+//     'http://admin.247itinternational.com',
+//     'http://www.247itinternational.com/'    
+//   ]
+//   var origin = req.headers.origin;
+//   app.logLevel1("origin >>> ", origin);
 
-  if (domains.indexOf(origin) > -1) {
-    app.logLevel1("origin found >>> ", origin);
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-
-
-  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  // res.setHeader('Access-Control-Allow-Origin', 'https://meditation-client.herokuapp.com');
+//   if (domains.indexOf(origin) > -1) {
+//     app.logLevel1("origin found >>> ", origin);
+//     res.setHeader('Access-Control-Allow-Origin', origin);
+//   }
 
 
-  //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//   // res.setHeader('Access-Control-Allow-Origin', 'https://meditation-client.herokuapp.com');
 
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+//   //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//   // Request methods you wish to allow
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-  // Pass to next layer of middleware
-  next();
-})
+//   // Request headers you wish to allow
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+
+//   // Pass to next layer of middleware
+//   next();
+// })
+
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
